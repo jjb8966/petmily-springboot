c058e74578c028588694d6ff80b6d07e52ff2f4a
jjb8966 <jjb8966@naver.com>
2022-08-10 20:55:10 +0900

    refactor: 데이터 확인 로그 추가 및 Mapper.xml 파일 이름 오탈자 수정

--- src/main/java/kh/petmily/controller/AbandonedAnimalController.java
+++ src/main/java/kh/petmily/controller/AbandonedAnimalController.java
@@ -80,13 +80,12 @@ public class AbandonedAnimalController {
     public String donate(@ModelAttribute DonateSubmitForm donateSubmitForm,
                          HttpServletRequest request) {
 
-        log.info("donateSubmitForm = {}", donateSubmitForm);
-
         Member member = getAuthMember(request);
-
         int mNumber = member.getMNumber();
 
         donateSubmitForm.setMNumber(mNumber);
+        log.info("donateSubmitForm = {}", donateSubmitForm);
+
         donateService.donate(donateSubmitForm);
 
         return "/abandoned_animal/submitSuccess";
