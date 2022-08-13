e89e3877f03a2a744d02946e5fcc2007df34972a
yuseful <yusefulfor@gmail.com>
2022-08-13 16:03:38 +0900

    feat: board 상세게시글 보기 및 삭제 controller

--- src/main/java/kh/petmily/controller/BoardController.java
+++ src/main/java/kh/petmily/controller/BoardController.java
@@ -1,15 +1,20 @@
 package kh.petmily.controller;
 
 import kh.petmily.domain.board.form.BoardPage;
+import kh.petmily.domain.board.form.BoardModifyForm;
+import kh.petmily.domain.board.form.ReadBoardForm;
+import kh.petmily.domain.member.Member;
 import kh.petmily.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/board")
@@ -19,7 +24,7 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/list")
-    public String list(HttpServletRequest request, Model model) throws Exception {
+    public String list(HttpServletRequest request, Model model) {
         String pbNumberVal = request.getParameter("pbNumber");
         String kindOfBoard = request.getParameter("kindOfBoard");
 
@@ -34,5 +39,29 @@ public class BoardController {
         model.addAttribute("kindOfBoard", kindOfBoard);
 
         return "/board/boardList";
+    }
+
+    private static Member getAuthMember(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        Member member = (Member) session.getAttribute("authUser");
+
+        return member;
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam("bNumber") int bNumber, Model model) {
+        ReadBoardForm detailForm = boardService.getBoard(bNumber);
+
+        model.addAttribute("detailForm", detailForm);
+
+        return "/board/boardDetailForm";
+    }
+
+
+    @GetMapping("/auth/delete")
+    public String delete(@RequestParam("bNumber") int bNumber){
+        boardService.delete(bNumber);
+
+        return "/board/deleteSuccess";
     }
 }
