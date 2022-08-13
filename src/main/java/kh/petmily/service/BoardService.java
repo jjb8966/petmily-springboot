a5dbb36692cee4b3bfe5a516a8b0a620466e8555
yuseful <yusefulfor@gmail.com>
2022-08-13 16:08:21 +0900

    feat: board 상세게시글 보기 및 삭제 service 구현

--- src/main/java/kh/petmily/service/BoardService.java
+++ src/main/java/kh/petmily/service/BoardService.java
@@ -1,10 +1,13 @@
 package kh.petmily.service;
 
 import kh.petmily.domain.board.form.BoardPage;
+import kh.petmily.domain.board.form.BoardModifyForm;
 import kh.petmily.domain.board.form.ReadBoardForm;
 
 public interface BoardService {
     public BoardPage getBoardPage(int pbNumber, String kindOfBoard);
 
+    public ReadBoardForm getBoard(int bNumber);
 
+    public void delete(int bNumber);
 }
--- src/main/java/kh/petmily/service/BoardServiceImpl.java
+++ src/main/java/kh/petmily/service/BoardServiceImpl.java
@@ -2,6 +2,7 @@ package kh.petmily.service;
 
 import kh.petmily.dao.BoardDao;
 import kh.petmily.domain.board.form.BoardPage;
+import kh.petmily.domain.board.form.BoardModifyForm;
 import kh.petmily.domain.board.form.ReadBoardForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,56 +24,25 @@ public class BoardServiceImpl implements BoardService {
         return new BoardPage(total, pageNum, size, content);
     }
 
+    @Override
+    public ReadBoardForm getBoard(int bNumber) {
+        ReadBoardForm readBoardForm = boardDao.selectByContent(bNumber);
 
-
-
-/*
-    public void modify(ModifyRequest modReq) {
-
-            ReadBoardForm readBoardForm = boardDao.selectByContent(modReq.getbNumber());
-
-            if (readBoardForm == null) {
-                throw new BoardNotFoundException();
-            }
-
-            if (!canModify(modReq.getmNumber(), readBoardForm)) {
-                throw new PermissionDeniedException();
-            }
-
-            boardDao.update(
-                    conn,
-                    modReq.getbNumber(),
-                    modReq.getTitle(),
-                    modReq.getContent(),
-                    modReq.getCheckPublic());
+        return new ReadBoardForm(
+                readBoardForm.getBNumber(),
+                readBoardForm.getMNumber(),
+                readBoardForm.getName(),
+                readBoardForm.getKindOfBoard(),
+                readBoardForm.getTitle(),
+                readBoardForm.getContent(),
+                readBoardForm.getWrTime(),
+                readBoardForm.getCheckPublic()
+        );
     }
 
-    private boolean canModify(int mNumber, ReadBoardForm readBoardForm) {
-        return readBoardForm.getmNumber() == mNumber;
+    @Override
+    public void delete(int bNumber) {
+        boardDao.delete(bNumber);
     }
 
-    private int size = 5;
-
-    public BoardPage getBoardPage(int pageNum, String kindOfBoard) {
-
-            int total = boardDao.selectCount(conn, kindOfBoard);
-            List<ReadBoardForm> content = boardDao.select(conn, (pageNum - 1) * size, size, kindOfBoard);
-    }
-
-    public void delete(int bNumber, User authUser) {
-
-            ReadBoardForm readBoardForm = boardDao.selectByContent(bNumber);
-
-            if (readBoardForm == null) {
-                throw new BoardNotFoundException();
-            }
-
-            if (!canModify(authUser.getmNumber(), readBoardForm)) {
-                throw new PermissionDeniedException();
-            }
-
-            boardDao.delete(bNumber);
-
-    }
-*/
 }
